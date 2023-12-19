1c23f4456a6bb74a1df7ccb8a8f99873086de1de
Vital1992 <vital09.92@gmail.com>
2023-12-19 10:52:43 -0800

    Add anoher subsete solution

--- Subsets/script.js
+++ Subsets/script.js
@@ -28,18 +28,41 @@
 const nums = [1,2,2]
 
 function findSubset(arr,curr){
-    console.log(arr)
-    console.log(curr)
     res.push([...curr])
-    for(let i=0;i<arr.length;i++){
-        if(i== 0 || arr[i] != arr[i-1]){
-            curr.push(arr[i])
-            findSubset(arr.slice(i+1),curr)
+    console.log(arr) // [ 1, 2, 2 ] // [2, 2] // [2] // []
+    console.log(curr) // [] // [1] // [1, 2] // [1, 2, 2]
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            curr.push(arr[i]) // [1] // [2] // [2]
+            findSubset(arr.slice(i + 1), curr) // [2, 2] [1] // [2] [1, 2] // [] [1, 2, 2]
             curr.pop()
         }
     }
 }
 let res = []
-nums.sort((a,b)=>{return a-b})
-findSubset(nums,[])
+nums.sort((a,b)=>{return a - b})
+findSubset(nums, [])
 console.log(res)
+
+//
+
+    const result = [];
+    nums.sort();
+    
+    const dfs = (arr, i) => {
+        if (i === nums.length) {
+            result.push(arr);
+            return;
+        }
+        // take the current number
+        dfs([...arr, nums[i]], i+1);
+        // while not at end of array and duplicate after current number
+        while (i+1 < nums.length && nums[i] === nums[i + 1]) {
+            i++;
+        }
+        // leave the current number
+        dfs([...arr], i+1);
+    }
+    
+    dfs([], 0);
+console.log(result)
